8067c9a3f8842126619dfaa3483f809cc7b78b14
Christian Tellez <atellezcastr@mun.ca>
2021-04-29 19:21:42 +0000

    Files and experiments

---
+++ BorealisAI/Unified/lrnni.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+import numpy as np
+import nni
+from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error
+import time
+
+class linearRegression(torch.nn.Module):
+    def __init__(self, inputSize, outputSize):
+        super(linearRegression, self).__init__()
+        self.linear = torch.nn.Linear(inputSize, outputSize)
+    def forward(self, x):
+        return self.linear(x)
+
+def train(model, opt, loss_fn, train_ld, n_epoch=3):
+    ##Iterate through a number of epochs
+    for epoch in range(n_epoch):
+        ##Training with batches of data
+        running_loss = 0.0
+        for i, data in enumerate(train_ld,0):
+            inputs, labels = data[:,:16], data[:,16:]
+            #First step: Generate predictions
+           
+            pred = model(inputs)
+           
+            #2nd step: Calculate loss
+            loss = loss_fn(pred, labels)
+            loss.backward()
+            opt.step()
+
+            opt.zero_grad()
+            running_loss += loss.item()
+            if i % 500 == 499:
+                nni.report_intermediate_result(running_loss)
+                running_loss = 0.0
+
+def test(testLoader, model):
+    #model = linearRegression(16,1)
+    #model.load_state_dict(torch.load(load_pretrained_weights))
+
+    offset = 0
+    total = 0
+    with torch.no_grad():
+        for data in testLoader:
+            #print(data)
+            predictions = model(data[:,:16]).numpy()
+            offset += mean_squared_error(data[:,16:], predictions)
+            total += 1
+            
+    result = offset / total
+    #print(result)
+    nni.report_final_result(result) 
+
+def main(args):
+    ## Load the data
+    data1, data2, data3 = pd.read_json('./data1.json'), pd.read_json('./data2.json'), pd.read_json('./data3.json')
+    frames = [data1, data2, data3]
+    data = pd.concat(frames, sort=False)
+    outputNorm = preprocessing.normalize([np.array(data['output'])])
+    data['output'] = outputNorm[0].tolist()
+
+    data = torch.from_numpy(data.values)
+    data = data.float()
+    #print(data)
+    ## Divide the data
+    testSize = int(0.1 * len(data))
+    trainSize = len(data)-testSize
+    
+    trainSet, testSet = torch.utils.data.random_split(data, [trainSize, testSize])
+    ## Load the data into tensor
+    trainLoader = torch.utils.data.DataLoader(trainSet, batch_size=args['batch_size'], num_workers=0)
+    testLoader = torch.utils.data.DataLoader(testSet, batch_size=args['batch_size'], num_workers=0)
+
+    ## Model and parameters
+    model = linearRegression(16,1) ## model = linearRegreggion(nn.Regression)
+    loss_fn = nn.MSELoss()
+    opt = torch.optim.SGD(model.parameters(), lr=args['lr'])
+
+    start = time.time()
+    train(model, opt, loss_fn, trainLoader, n_epoch=args['epochs'])
+    run_time = time.time() - start
+    #print(run_time)
+    nni.report_intermediate_result(run_time)
+    test(testLoader, model)
+
+if __name__ == '__main__':
+    params = {'batch_size':16, 'epochs': 100, 'lr':1e-5}
+    params = nni.get_next_parameter()
+    main(params)
---
+++ BorealisAI/Unified/rgbtnni.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+import numpy as np
+import nni
+import time
+
+def main(args):
+    data1, data2, data3 = pd.read_json('./data1.json'), pd.read_json('./data2.json'), pd.read_json('./data3.json')
+
+    frames = [data1, data2, data3]
+    data = pd.concat(frames, sort=False)
+    outputNorm = preprocessing.normalize([np.array(data['output'])])
+    data['output'] = outputNorm[0].tolist()
+    #print(data.head)
+    trainSet, testSet = train_test_split(data, test_size=0.1)
+
+    regressor = xgb.XGBRegressor(
+        n_estimators = args['n_estimators'],
+        reg_lambda=args['lambda'],
+        gamma = 0,
+        max_depth=args['depth']
+    )
+    start = time.time()
+    regressor.fit(trainSet.iloc[:,:16], trainSet.iloc[:,16:])
+    run_time = time.time() - start
+    nni.report_intermediate_result(run_time)
+    nni.report_final_result(mean_squared_error(testSet.iloc[:,16:], regressor.predict(testSet.iloc[:,:16])))
+
+if __name__ == '__main__':
+    params = {'n_estimators': 100, 'lambda':1e-1, 'depth': 15}
+    params = nni.get_next_parameter()
+    main(params)
